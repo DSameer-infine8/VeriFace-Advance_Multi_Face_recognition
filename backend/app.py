86d74198e1788de447bd742498fba7b58d30e346
SamInfine8 <sameer8creative.tech@gmail.com>
2026-07-14 12:37:46 +0530

    Setup Flask backend and connect to frontend

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,24 @@
-import os 
+import os
+import sys
 import pathlib as Path 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
+
+BASE_DIR = os.path.dirname(__file__)
+
+app = Flask(
+    __name__,
+    template_folder=os.path.join(BASE_DIR, "..", "frontend"),
+    static_folder=os.path.join(BASE_DIR, "..", "frontend"),
+    static_url_path=""
+)
+
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
